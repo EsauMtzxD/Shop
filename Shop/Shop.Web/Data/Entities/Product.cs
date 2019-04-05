864d7f78fb0220fe22df979a88efebaa82db3fef
Esau Mtz <mtzab6@gmail.com>
2019-04-04 20:02:58 -0600

    Implemetn Repository Pattern

---
+++ Shop/Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+
+    public class Product
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImgeUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime? LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime? LastSale { get; set; }
+
+        [Display(Name = "Is Avalible")]
+        public bool IsAvalible { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+    }
+}
---
+++ Shop/Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,47 @@
+﻿using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data
+{
+    public class SeedDb
+    {
+
+        private readonly DataDbContext context;
+        private Random random;
+
+        public SeedDb(DataDbContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("First Product");
+                this.AddProduct("Second Product");
+                this.AddProduct("Third Product");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(100),
+                IsAvalible = true,
+                Stock = this.random.Next(100)
+            });
+        }
+
+
+    }
+}
